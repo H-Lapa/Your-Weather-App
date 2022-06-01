e8806bf8ba29509cd41ce7b6af8b8a601270b291
Hugo Lapa <Hugolapa888@gmail.com>
2022-06-01 15:12:31 +0100

    Icon code fixed

--- components/WeatherIcon.js
+++ components/WeatherIcon.js
@@ -4,51 +4,9 @@ const iconLink = (code) => {
     return `http://openweathermap.org/img/wn/${code}@2x.png`;
 }
 
-const getCode = (WeatherDesc, sunset) => {
-    let code = "";
-    switch(WeatherDesc) {
-        case "clear sky":
-          code = "01";
-          break;
-        case "few clouds":
-            code = "02";
-          break;
-        case "scattered clouds":
-            code = "03";
-          break;
-        case "broken clouds":
-        case "overcast clouds":
-            code = "04";
-          break;
-        case "shower rain":
-            code = "09";
-          break; 
-        case "rain":
-            code = "10";
-          break;
-        case "thunderstorm":
-            code = "11";
-          break;
-        case "snow":
-            code = "13";
-          break;
-        case "mist":
-            code = "50";
-          break;
-    }
-
-    if (new Date() > sunset) {
-        code += "n";
-    } else {
-        code += "d";
-    }
-
-    return code;
-}
-
-const WeatherIcon = ({WeatherDesc, sunset}) => {
+const WeatherIcon = ({code, WeatherDesc}) => {
     return (
-        <img src={iconLink(getCode(WeatherDesc, sunset))} alt={WeatherDesc} />
+        <img src={iconLink(code)} alt={WeatherDesc} />
     )
 }
 
--- components/weatherCard.js
+++ components/weatherCard.js
@@ -8,7 +8,9 @@ const WeatherCard = ({obj}) => {
             <div className={styles.card}>
                 <h1 > Temp: { obj.data.main.temp } </h1>
                 <h2>Feels Like: {obj.data.main.feels_like }</h2>
-                <WeatherIcon WeatherDesc={obj.data.weather[0].description} sunset={obj.data.sys.sunset} />
+
+                <WeatherIcon code={obj.data.weather[0].icon} WeatherDesc={obj.data.weather[0].desciption} />
+
                 <h3>range {obj.data.main.temp_max} / {obj.data.main.temp_min } </h3>
                 <h4>humidity: {obj.data.main.humidity} , pressure: {obj.data.main.pressure} </h4>
                 <h4>description: {obj.data.weather[0].description} </h4>
